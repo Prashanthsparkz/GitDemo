6882450a9950135b3850d047391822d428d7e4ab
prashanth <prashanthsparkz@gmail.com>
2024-05-07 21:56:02 +0530

    first commit

---
+++ src/test_project/ResultSearchApp.java
@@ -0,0 +1,52 @@
+package test_project;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ResultSearchApp {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rollNumbers: ");
+        int n = scanner.nextInt();
+
+        int[] rollNumbers = new int[n];
+
+        System.out.println("Enter the rollNumbers (in sorted order):");
+        for (int i = 0; i < n; i++) {
+            rollNumbers[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(rollNumbers);
+
+      
+        System.out.print("Enter your roll number: ");
+        int rollNumberToSearch = scanner.nextInt();
+
+    
+        String result = searchRollNumber(rollNumbers, rollNumberToSearch);
+
+     
+        System.out.println("Result for Roll Number " + rollNumberToSearch + ": " + result);
+    }
+
+   
+    private static String searchRollNumber(int[] rollNumbers, int target) {
+        int low = 0;
+        int high = rollNumbers.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (rollNumbers[mid] == target) {
+                return "Pass";
+            } else if (rollNumbers[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return "Failed";
+    }
+}
---
+++ src/test_project/TestProject.java
@@ -0,0 +1,45 @@
+package test_project;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * int a=30; float b=1.34f; boolean c = true; String d ="Prashanth";
+		 * 
+		 * int p=10; int q=20; int r=p+q;
+		 * 
+		 * 
+		 * System.out.println("I am a programmer"); System.out.println(a);
+		 * System.out.println(b); System.out.println(c); System.out.println(d);
+		 * 
+		 * System.out.println(r); System.out.println(d);
+		 */
+		
+		/*
+		 * if(10>8) System.out.println("10 is greater than 8");
+		 */
+		
+		/*if blocks
+		 int x=10;
+		int y=20;
+		if (x>y) {
+			System.out.println("x is greater than y");
+		}
+		else{
+			System.out.println("y is greater than x");
+		}*/
+		
+		/*
+		 * int f=1; while(f<=4) { System.out.println(f); f++; }
+		 */
+		
+		for (int i=0;i<5;i++) {
+			for (int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
